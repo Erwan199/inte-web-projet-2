892e8c1d9a732a62e2cbd849e927575a1558703f
Erwan <erwan.battais@ynov.com>
2018-12-07 12:22:47 +0100

    ajout php

---
+++ mail.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Envoi d'un email par formulaire</title>
+    </head>
+    <body>
+        <p>
+        <?php
+        $retour = mail('erwan.battais@ynov.com', 'Envoi depuis le site test', $_POST['message'], 'From : webmaster@free.fr');
+        if ($retour)
+            echo 'Votre message a bien été envoyé !';
+        ?>
+        </p>
+    </body>
+</html>
